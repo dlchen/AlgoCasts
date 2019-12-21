d928d72dd27cad44c496cb9e86463ba19eb64636
dlchen <dan.zot@gmail.com>
2019-12-21 10:16:28 -0800

    matrix: recursive solution #2

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -18,43 +18,61 @@
 function matrix(n) {
 
   const m = new Array(n).fill(null).map(_ => []);
+  const target = n * n + 1;
 
-  let val = 1;
-  let start_col = 0;
-  let end_col = n - 1;
-  let start_row = 0;
-  let end_row = n - 1;
+  function fillStartRow(val, i, start_col, end_col, start_row, end_row) {
 
-  while (start_col <= end_col && start_row <= end_row) {
+    if (val === target) return;
 
-    for (let i = start_col; i <= end_col; i++) {
-      m[start_row][i] = val;
-      val += 1;
-    }
-    start_row += 1;
+    if (i > end_col) {
+      return fillEndCol(val, start_row + 1, start_col, end_col, start_row + 1, end_row);
+    };
 
-    for (let i = start_row; i <= end_row; i++) {
-      m[i][end_col] = val;
-      val += 1;
-    }
-    end_col -= 1;
-
-    for (let i = end_col; i >= start_col; i--) {
-      m[end_row][i] = val;
-      val += 1;
-    }
-    end_row -= 1;
-
-    for (let i = end_row; i >= start_row; i--) {
-      m[i][start_col] = val;
-      val += 1;
-    }
-    start_col += 1;
+    m[start_row][i] = val;
+    fillStartRow(val + 1, i + 1, start_col, end_col, start_row, end_row)
   }
+
+  function fillEndCol(val, i, start_col, end_col, start_row, end_row) {
+
+    if (val === target) return;
+
+    if (i > end_row) {
+      return fillEndRow(val, end_col - 1, start_col, end_col - 1, start_row, end_row);
+    }
+
+    m[i][end_col] = val;
+    fillEndCol(val + 1, i + 1, start_col, end_col, start_row, end_row)
+  }
+
+  function fillEndRow(val, i, start_col, end_col, start_row, end_row) {
+
+    if (val === target) return;
+
+    if (i < start_col) {
+      return fillStartCol(val, end_row - 1, start_col, end_col, start_row, end_row - 1);
+    }
+
+    m[end_row][i] = val;
+    fillEndRow(val + 1, i - 1, start_col, end_col, start_row, end_row)
+  }
+
+  function fillStartCol(val, i, start_col, end_col, start_row, end_row) {
+
+    if (val === target) return;
+
+    if (i < start_row) {
+      return fillStartRow(val, start_col + 1, start_col + 1, end_col, start_row, end_row);
+    }
+
+    m[i][start_col] = val;
+    fillStartCol(val + 1, i - 1, start_col, end_col, start_row, end_row)
+  }
+
+  fillStartRow(1, 0, 0, n - 1, 0, n - 1);
 
   return m;
 }
 
-// console.log(matrix(3));
+console.log(matrix(3));
 
 module.exports = matrix;
