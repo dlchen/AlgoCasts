247a12102a31503b701f790fcebf569956551ce8
dlchen <dan.zot@gmail.com>
2019-12-24 09:14:25 -0800

    fib: recursive solution #1

--- exercises/fib/index.js
+++ exercises/fib/index.js
@@ -3,6 +3,7 @@
 // The fibonacci series is an ordering of numbers where
 // each number is the sum of the preceeding two.
 // For example, the sequence
+//   0  1  2  3  4  5
 //  [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 // forms the first ten entries of the fibonacci series.
 // Example:
@@ -12,18 +13,22 @@ function fib(n) {
 
   if (n <= 1) return n;
 
-  let a = 0
+  let a = 0;
   let b = 1;
-  let c;
-  for (let i = 2; i <= n; i++) {
-    c = a + b;
+
+  const loop = (i) => {
+
+    const c = a + b;
     a = b;
     b = c;
+    
+    if (i === n) return c;
+    return loop(i + 1);
   }
 
-  return c;
+  return loop(2);
 }
 
-// console.log(fib(3))
+// console.log(fib(39))
 
 module.exports = fib;
