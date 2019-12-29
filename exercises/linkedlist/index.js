09971f2f69210bce99984073e7922c020834f115
dlchen <dan.zot@gmail.com>
2019-12-29 09:55:27 -0800

    linkedlist: size iterative

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,8 +2,31 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, next = null) {
+    this.data = data;
+    this.next = next;
+  }
+}
 
-class LinkedList {}
+class LinkedList {
+  constructor() {
+    this.head = null;
+  }
+
+  insertFirst(data) {
+    this.head = new Node(data, this.head);
+  }
+
+  size() {
+    let count = 0;
+    let curr = this.head;
+    while (curr) {
+      count++;
+      curr = curr.next;
+    }
+    return count;
+  }
+}
 
 module.exports = { Node, LinkedList };
