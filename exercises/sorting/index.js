6cafec1d62d407c460ed21b79993dc0fd5d260cd
dlchen <dan.zot@gmail.com>
2020-01-17 12:11:16 -0800

    sorting: merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -35,10 +35,36 @@ function selectionSort(arr) {
 
 function mergeSort(arr) {
 
+  if (arr.length <= 1) return arr; 
+
+  const midpoint = Math.floor(arr.length / 2);
+  const front = arr.slice(0, midpoint);
+  const back = arr.slice(midpoint);
+
+  const sortedFront = mergeSort(front);
+  const sortedBack = mergeSort(back);
+  return merge(sortedFront, sortedBack);
 }
 
 function merge(left, right) {
 
+  const loop = (merged, left, right) => {
+
+    if (!left.length && !right.length) return merged;
+    if (!left.length) return merged.concat(right);
+    if (!right.length) return merged.concat(left);
+    const headLeft = left[0];
+    const tailLeft = left.slice(1);
+    const headRight = right[0];
+    const tailRight = right.slice(1);
+    if (headLeft < headRight) {
+      return loop(merged.concat([headLeft]), tailLeft, right);
+    } else {
+      return loop(merged.concat([headRight]), left, tailRight);
+    }
+  }
+
+  return loop([], left, right);
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
